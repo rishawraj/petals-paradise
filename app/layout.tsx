5f64c9e2a6b0aad65a6f442ea45c4a39eab3f6f1
rishawraj <rishawraj0703@gmail.com>
2024-04-07 22:07:42 +0530

    build: implement prettier and configure eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Petals",
-  description: "Best place to get your green friend.",
+  title: 'Petals',
+  description: 'Best place to get your green friend.',
 };
 
 export default function RootLayout({
