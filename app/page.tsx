0501054b8f71bfa595cc4d4bc632de2237478c24
rishawraj <rishawraj0703@gmail.com>
2024-04-08 17:12:11 +0530

    feat: add shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <>
       <h1 className="text-4xl">Hello Petals</h1>
+      <Button variant="default">Hello</Button>
     </>
   );
 }
