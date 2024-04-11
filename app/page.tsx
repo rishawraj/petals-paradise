e093a1c9308b3a1ab58ac5c4404a87a9f35608e4
rishawraj <rishawraj0703@gmail.com>
2024-04-11 22:59:35 +0530

    feat: add side-menu and style the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
+'use client';
+
 import cartIcon from '@/components/icons/cartIcon.svg';
 import leafLogoIcon from '@/components/icons/leafLogoIcon.svg';
 import searchIcon from '@/components/icons/searchIcon.svg';
 import userIcon from '@/components/icons/userIcon.svg';
+import { MenuIcon, XIcon } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const [scrollDisabled, setScrollDisabled] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+    setScrollDisabled(!scrollDisabled);
+    document.body.style.overflow = scrollDisabled ? 'auto' : 'hidden';
+  };
+
   return (
     <>
       {/* <BreakPoint /> */}
-      <div className="flex ">
+      <div className="flex min-h-screen flex-col pb-5 sm:flex-row">
         {/* navbar */}
-        <div className=" border-green-5000 border-  absolute mt-5 h-12 w-full bg-transparent xl:mt-8">
+        <div className="border-green-5000 border- absolute  mt-5 h-12 w-full  bg-transparent xl:mt-8">
           <div className="border-rose-4000 border- mx-auto flex w-full justify-between">
-            <ul className="border-amber-4000 border- flex w-3/5 justify-evenly px-5">
-              <li className="flex gap-1">
+            <ul className="border-amber-4000 border- flex w-3/5 px-5 md:justify-evenly">
+              <li className="z-20 flex gap-1">
                 <Image
                   src={leafLogoIcon}
                   height={24}
@@ -22,14 +35,14 @@ export default function Home() {
                 />
                 <span className="font-bold">Petals</span>
               </li>
-              <div className=" bg-red-2000 flex w-2/3 justify-evenly font-semibold text-muted-foreground">
+              <div className=" bg-red-2000  hidden w-2/3 font-semibold text-muted-foreground md:flex md:justify-evenly">
                 <li className="text-primary">Home</li>
                 <li>About</li>
                 <li>Products</li>
                 <li>Contact</li>
               </div>
             </ul>
-            <ul className="border-purple-4000 border- flex w-2/5 justify-end gap-5 pr-[5%] ">
+            <ul className="border-purple-4000 border- hidden w-2/5 justify-end gap-5 pr-[5%] md:flex ">
               <li>
                 <Image
                   src={searchIcon}
@@ -45,12 +58,87 @@ export default function Home() {
                 <Image src={userIcon} width={24} height={24} alt="user-icon" />
               </li>
             </ul>
+            <button onClick={handleMenuClick} className=" pr-5 md:hidden">
+              {<MenuIcon />}
+            </button>
           </div>
         </div>
 
-        {/* text main2 */}
-        <div className="bg-red-2000 bg-rose-2000  mt-16 flex min-h-full w-full flex-[3] flex-col items-start justify-center pl-[6%] ">
-          <p className="text-sm font-semibold text-muted-foreground">
+        {/* side menu */}
+        {isSideMenuOpen && (
+          <div
+            style={{
+              height: '1000px',
+              overflowY: 'scroll',
+              scrollbarWidth: scrollDisabled ? 'auto' : 'thin', // Hide the scrollbar thumb
+              msOverflowStyle: 'none', // Hide scrollbar in IE and Edge
+            }}
+            className="absolute left-0 top-0 z-10 flex h-screen w-full flex-col items-center justify-start  bg-white p-5 md:hidden"
+          >
+            {/* overlay bg-image sm */}
+            <div className="absolute left-0 top-0 -z-10 h-full w-full bg-white opacity-80 md:hidden"></div>
+            <div className="absolute left-0 top-0 -z-20 h-full w-full bg-[url('/image-2.png')] bg-contain bg-center bg-no-repeat md:hidden"></div>
+            {/*  */}
+            <div className="bg-rose-2000 mb-10 flex w-full justify-end">
+              <button onClick={handleMenuClick}>{<XIcon />}</button>
+            </div>
+
+            <div className="flex flex-col justify-center p-5">
+              <ul className="mb-10 flex flex-col justify-center gap-10">
+                <button className="border-b-2 text-4xl hover:border-black">
+                  Home
+                </button>
+                <button className="border-b-2 text-4xl hover:border-black">
+                  About
+                </button>
+                <button className="border-b-2 text-4xl hover:border-black">
+                  Products
+                </button>
+                <button className="border-b-2 text-4xl hover:border-black">
+                  Contact
+                </button>
+              </ul>
+            </div>
+
+            <div className="bg-rose-3000 pr-5">
+              <ul className="flex  justify-center gap-10">
+                <li>
+                  <Image
+                    src={searchIcon}
+                    width={24}
+                    height={24}
+                    alt="search-icon"
+                  />
+                </li>
+                <li>
+                  <Image
+                    src={cartIcon}
+                    width={24}
+                    height={24}
+                    alt="cart-icon"
+                  />
+                </li>
+                <li>
+                  <Image
+                    src={userIcon}
+                    width={24}
+                    height={24}
+                    alt="user-icon"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+
+        {/* text main */}
+        <div className="bg-red-2000 bg-rose-2000 bg-red-5000 mt-16 flex min-h-full w-full flex-[3] flex-col items-start  justify-center pl-[6%] ">
+          {/* overlay bg-image sm */}
+          <div className="absolute left-0 top-0 -z-10 h-full w-full bg-white opacity-80 md:hidden"></div>
+          <div className="absolute left-0 top-0 -z-20 h-full w-full bg-[url('/image-2.png')] bg-contain bg-center bg-no-repeat md:hidden"></div>
+          {/*  */}
+
+          <p className="mt-5 text-sm font-semibold text-muted-foreground opacity-100">
             INDOOR PLANTS
           </p>
 
@@ -111,7 +199,7 @@ export default function Home() {
               <p className="text-sm font-semibold text-gray-600">shop now + </p>
             </div>
             {/*  */}
-            <div className="border- border-blac bg-slate-4000 ">
+            <div className="bg-slate-4000">
               <div className="bg-green-3000 relative mr-5 flex  min-h-40 min-w-40 flex-col items-center justify-center bg-slate-200">
                 <Image
                   src="/indoor-plant-2.png"
@@ -123,26 +211,33 @@ export default function Home() {
                 />
               </div>
               <p className="mt-2 text-lg font-semibold">Snake Leaves</p>
-              <p className="text-sm font-semibold text-gray-600">shop now + </p>
+              <button className="text-sm font-semibold text-gray-600 hover:underline">
+                shop now +{' '}
+              </button>
             </div>
             {/*  */}
           </div>
 
+          {/* image and layer */}
+
           {/*  */}
         </div>
 
-        {/* image */}
-        <div className="bg-slate-2000 bg-slate-2000 min-h-screen w-full flex-[2] bg-gradient-to-bl from-slate-100 to-slate-300">
-          <div className="bg-rose-2000 bg-slate-2000 relative flex h-full w-full items-center justify-center">
+        {/* plant image */}
+        <div className="bg-slate-2000 bg-slate-2000 hidden min-h-screen w-full flex-[2] bg-gradient-to-bl from-slate-100 to-slate-300 md:block">
+          <div className="relative h-full w-full">
             <Image
-              src="/image-2.png"
+              src={'/image-2.png'}
               fill={true}
-              alt="plant image for home page"
-              style={{ objectFit: 'contain', padding: '12%' }}
+              alt={'plant'}
+              style={{ objectFit: 'contain', padding: '10px' }}
             />
           </div>
         </div>
       </div>
+
+      {/*  */}
+      <div className=" h-screen bg-red-400"></div>
     </>
   );
 }
